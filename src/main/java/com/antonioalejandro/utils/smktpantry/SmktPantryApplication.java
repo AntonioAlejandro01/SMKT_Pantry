99ef84aad89a3310a6156931a6a806d2100099b5
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2021-03-27 20:55:24 +0100

    IC

---
+++ src/main/java/com/antonioalejandro/utils/smktpantry/SmktPantryApplication.java
@@ -0,0 +1,13 @@
+package com.antonioalejandro.utils.smktpantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmktPantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmktPantryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonioalejandro/utils/smktpantry/SmktPantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonioalejandro.utils.smktpantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmktPantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
